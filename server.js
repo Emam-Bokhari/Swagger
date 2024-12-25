b09d8d4730d0b11abcc79e4a6cc886459b207c87
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-25 22:43:05 +0600

    feat: Implemented user data endpoint with Swagger UI integration

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+// create express application
+const app = express();
+const port = 3000;
+
+// load YAML file
+const swaggerDocument = YAML.load("./learning/basic/get.yaml");
+
+// Swagger UI route setup
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+//  API endpoints
+app.get("/users", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      fullName: "John Doe",
+      email: "john.doe@example.com",
+      role: "user",
+      isDeleted: false,
+      createdAt: "2024-12-23T15:30:00Z",
+      updatedAt: "2024-12-23T15:30:00Z",
+    },
+    {
+      id: 2,
+      fullName: "Jane Doe",
+      email: "jane.doe@example.com",
+      role: "user",
+      isDeleted: false,
+      createdAt: "2024-12-23T15:30:00Z",
+      updatedAt: "2024-12-23T15:30:00Z",
+    },
+  ]);
+});
+
+// API স্রোত শুরু
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
